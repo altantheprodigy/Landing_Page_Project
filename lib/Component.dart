b81584f9c7086cd8915e935fa5627d8aa0caae11
altantheprodigy <altanpoke9@sar.ac.id>
2023-11-06 14:40:27 +0700

    feat: [AP] fitur Promo

--- lib/Component.dart
+++ lib/Component.dart
@@ -524,5 +524,3 @@ class judulPage extends StatelessWidget {
     );
   }
 }
-
-
--- lib/Shopping Page/PopularPage.dart
+++ lib/Shopping Page/PopularPage.dart
@@ -25,8 +25,66 @@ class _popularPageState extends State<popularPage> {
           shopCon(imagePath: 'images/sehat.png', title: "BPJS", imageAssetPath: "images/bpjs.png"),
           shopCon(imagePath: 'images/banyu.png', title: "PDAM", imageAssetPath: "images/pam.png"),
           SizedBox(height: 20,),
-          judulPage(title: "Promo", title1: "View more")
-        ],
+          judulPage(title: "Promo", title1: "View more"),
+      Container(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          children: [
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(6),
+                  color: AppColors.card,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Flexible(
+                      flex: 1,
+                      child: Container(
+                        child: Container(
+                          height: 60,
+                          clipBehavior: Clip.hardEdge,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(topLeft: Radius.circular(5), bottomLeft: Radius.circular(5)),
+                          ),
+                          child: Image.asset("images/Rectangle 55.png", fit: BoxFit.cover,),
+                        ),
+                      ),
+                    ),
+                    Flexible(
+                      flex: 3,
+                      child: Container(
+                        height: 60,
+                        clipBehavior: Clip.hardEdge,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(topRight: Radius.circular(5), bottomRight: Radius.circular(5)),
+                        ),
+                        child: Image.asset(
+                          "images/pam.png",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Flexible(
+                      flex: 1,
+                        child: Container(
+                          height: 60,
+                      decoration: BoxDecoration(
+                        color: AppColors.Green,
+                        borderRadius: BorderRadius.only(bottomRight: Radius.circular(5), topRight: Radius.circular(5))
+                      ),
+                    )
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      )
+
+      ],
       ),
     );
   }
